cd7d12325285537f7a9a0315f45ef0d8aee5e86f
Virginia <virginia.rabello2107@gmail.com>
2021-05-11 09:40:13 -0500

    generate webpage complete

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-  printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
